4f2f275c7a69707df16245bf3fc503ea38cbdf09
jegco <jegco13@gmail.com>
2022-08-07 03:25:36 -0500

    se limpia el archivo de propiedades de la aplicacion y se agrega valores faltantes para conexion con dropbox

--- src/main/java/com/tesis/transito/persistencia/utils/archivos/DropboxConfig.java
+++ src/main/java/com/tesis/transito/persistencia/utils/archivos/DropboxConfig.java
@@ -1,6 +1,7 @@
 package com.tesis.transito.persistencia.utils.archivos;
 
 import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.oauth.DbxCredential;
 import com.dropbox.core.v2.DbxClientV2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -12,9 +13,21 @@ public class DropboxConfig {
     @Value("${dropbox.accessToken}")
     private String accessToken;
 
+    @Value("${dropbox.refreshToken}")
+    private String refreshToken;
+
+    @Value("${dropbox.app.key}")
+    private String appKey;
+
+    @Value("${dropbox.app.secret}")
+    private String appSecret;
+
     @Bean
     public DbxClientV2 dropboxClient() {
         DbxRequestConfig config = DbxRequestConfig.newBuilder("transito").build();
-        return new DbxClientV2(config, accessToken);
+
+        DbxCredential credential = new DbxCredential(accessToken, -1L, refreshToken, appKey, appSecret);
+
+        return new DbxClientV2(config, credential);
     }
 }
